ce4a0247becdf899293c91bddf87efce1de18e23
rolandpo <rolopod@gmail.com>
2022-10-03 12:25:48 +0200

    fix email

--- pages/api/mailing-list/[path].js
+++ pages/api/mailing-list/[path].js
@@ -1,21 +1,24 @@
 export default async function handler(req, res) {
   try {
+    const { path } = req.query
+    if (req.method === 'POST') {
       const searchParams = new URLSearchParams()
       searchParams.set('email', req.body.email)
       const { path } = req.query
-      const data = await fetch(`${process.env.NEXT_PUBLIC_STATS_ENDPOINT}/mailing-list/${path}`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        referrerPolicy: 'no-referrer',
-        body: searchParams,
-      })
-      const json = await data.json()
-      res.status(200).json(json)
+      const endpoint = `${process.env.NEXT_PUBLIC_STATS_ENDPOINT}/mailing-list/${path}`
+      const data = await fetch(endpoint,
+        {
+          method: 'POST',
+          body: searchParams
+        }
+      ).then((r) => r.json())
+      res.status(200).json(data)
+    }
+    else {
+      const endpoint = `${process.env.NEXT_PUBLIC_STATS_ENDPOINT}/mailing-list/${path}`
+      const data = await fetch(endpoint).then((r) => r.json())
+      res.status(200).json(data)
+    }
   } catch (error) {
       console.error(error)
       return res.status(error.status || 500).end(error.message)
