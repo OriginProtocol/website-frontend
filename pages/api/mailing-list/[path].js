630e228b947882a142a3368214f31a52f9934cbc
rolandpo <rolopod@gmail.com>
2022-10-03 13:09:11 +0200

    fix email

--- pages/api/mailing-list/[path].js
+++ pages/api/mailing-list/[path].js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const { path } = req.query
+    const { path, email } = req.query
     if (req.method === 'POST') {
       const searchParams = new URLSearchParams()
       searchParams.set('email', req.body.email)
@@ -15,9 +15,8 @@ export default async function handler(req, res) {
       res.status(200).json(data)
     }
     else {
-      const endpoint = `${process.env.NEXT_PUBLIC_STATS_ENDPOINT}/mailing-list/${path}`
-      const data = await fetch(endpoint).then((r) => r.json())
-      res.status(200).json(data)
+      const endpoint = `${process.env.NEXT_PUBLIC_STATS_ENDPOINT}/mailing-list/${path}${email ? `?email=${email}` : ''}`
+      res.redirect(endpoint)
     }
   } catch (error) {
       console.error(error)
