38813724d8716cd4f370086f0d666f99a6cfa26b
Nick Hong <nickhong@originprotocol.com>
2022-10-05 12:26:11 -0400

    Remove unnecessary console log

--- src/components/strapi/blocks/RichText.js
+++ src/components/strapi/blocks/RichText.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactMarkdown from "react-markdown";
 
 const RichText = ({body}) => {
-  console.log(body)
   return (
     <>
       <ReactMarkdown children={body} />
