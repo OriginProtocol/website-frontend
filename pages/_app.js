8806c22fdac1b730006f725c825add85f9ae6fe8
rolandpo <rolopod@gmail.com>
2022-10-07 13:39:07 +0200

    add page analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,73 @@
 import App from "next/app";
 import Head from "next/head";
-import { createContext } from "react";
+import React, { createContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { fetchAPI } from "../lib/api";
 import { getStrapiMedia } from "../lib/media";
 import 'react-toastify/scss/main.scss'
 import '../styles/globals.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import analytics from 'utils/analytics'
+import { AnalyticsProvider } from 'use-analytics'
+import { setUserSource } from 'utils/user'
 
 const queryClient = new QueryClient()
 export const GlobalContext = createContext({});
 
 const MyApp = ({ Component, pageProps }) => {
   const { global } = pageProps;
+  const router = useRouter()
+
+  const trackPageView = (url, lastURL) => {
+    const data = {
+      toURL: url,
+    }
+
+    if (lastURL) {
+      data.fromURL = lastURL
+    }
+
+    analytics.page(data)
+
+    if (url.indexOf('?') > 0) {
+      const searchParams = new URLSearchParams(url.substr(url.indexOf('?') + 1))
+      const utmSource = searchParams.get('utm_source')
+      if (utmSource) {
+        setUserSource(utmSource)
+      }
+    } else {
+      /* if first page load is not equipped with the 'utm_source' we permanently mark
+       * user source as unknown
+       */
+      setUserSource('unknown')
+    }
+  }
+
+  useEffect(() => {
+    let lastURL = window.location.pathname + window.location.search
+
+    // track initial page load
+    trackPageView(lastURL)
+
+    const handleRouteChange = (url) => {
+      /* There is this weird behaviour with react router where `routeChangeComplete` gets triggered
+       * on initial load only if URL contains search parameters. And without this check and search
+       * parameters present the inital page view would be tracked twice.
+       */
+      if (url === lastURL) {
+        return
+      }
+      // track when user navigates to a new page
+      trackPageView(url, lastURL)
+      lastURL = url
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
 
   return (
     <>
@@ -23,12 +79,14 @@ const MyApp = ({ Component, pageProps }) => {
       </Head>
       <GlobalContext.Provider value={global.attributes}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <AnalyticsProvider instance={analytics}>
+          <Component {...pageProps} />
+        </AnalyticsProvider>
         </QueryClientProvider>
       </GlobalContext.Provider>
     </>
-  );
-};
+  )
+}
 
 // getInitialProps disables automatic static optimization for pages that don't
 // have getStaticProps. So article, category and home pages still get SSG.
