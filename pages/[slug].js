6fbc07fd416cfcbcdce94d6b394f49290946c2cc
Shahul Hameed <shahthepro@gmail.com>
2022-10-14 00:14:07 +0530

    Replace /slugs

--- pages/[slug].js
+++ pages/[slug].js
@@ -7,11 +7,11 @@ const FallbackRenderer = ({ article }) => {
 };
 
 export async function getStaticPaths() {
-  const { data } = await fetchAPI("/website/blog/slugs");
+  const { data } = await fetchAPI("/website/blog/en");
 
   return {
-    paths: (data || []).map((slug) => ({
-      params: { slug },
+    paths: (data || []).map((post) => ({
+      params: { slug: post.slug },
       // TODO: Should all locales be pre-generated?
       locale: "en",
     })),
