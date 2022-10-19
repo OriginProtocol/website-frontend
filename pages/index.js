cb118d0aeee6569ad845c892e764996692ac6825
Nick Hong <me@nickhong.xyz>
2022-10-19 15:17:06 -0400

    Lazy load youtube embed

--- pages/index.js
+++ pages/index.js
@@ -267,8 +267,10 @@ const Home = ({ locale, onLocale, isMobile, articles, seo, navLinks }) => {
                     width={`${isMobile ? "280" : "560"}`}
                     height={`${isMobile ? "158" : "316"}`}
                     src="https://www.youtube.com/embed/ERh2n-vlpQ4"
+                    srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/ERh2n-vlpQ4?autoplay=1><img src=https://img.youtube.com/vi/ERh2n-vlpQ4/hqdefault.jpg alt='Video Working at Origin'><span>▶</span></a>"
+                    loading="lazy"
                     className={`${styles.iframe}`}
-                    title="YouTube video player"
+                    title="Working at Origin"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
